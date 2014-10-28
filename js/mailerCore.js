707040e1081174ad5b9ad7bdd4c5cd5ece8d9098
jcoulterdesign <jcoulterdesign@gmail.com>
2014-10-28 00:09:38 +0000

    Code Tidy
    Tidied up the code

--- js/mailerCore.js
+++ js/mailerCore.js
@@ -1,76 +1,79 @@
 /* Cobalt Smart Mailer Core */
 
 $(document).ready(function(){
-
-
-$('ul').sortable({
-    items:'li',
-	axis: 'y'
-})
+	
+	var editableElem = 'p,h1,h2,h3,h4'; // These HTML elems will be editable within the template
+	var modalDur = 200; // Set the timing of the modal fade ins
+	
+	/* Make email elements sortable on the y axis */
+	$('ul').sortable({
+		items:'li',
+		axis: 'y'
+	});
 
 	// Make table row active on hover
 	$('.sTable').mouseenter(function(){
-		$('body').find('.activeHover').removeClass('activeHover')
-		$(this).addClass('activeHover')
-		   
+		$('body').find('.activeHover').removeClass('activeHover'); // Remove all other active classes
+		$(this).addClass('activeHover'); // Add active class to current element
 	});
+	
+	// Append the side toolbox on elem mouse over
 	$('.sTable').mouseenter(function(){
-	$('body').find('.toolbox').remove();
-		$(this).append('<div class="toolbox"><div class="tDelete">Delete</div><div class="tClone">Clone</div></div>')      
+		$('body').find('.toolbox').remove(); // Remove any other instances of the tool bar
+		$(this).append('<div class="toolbox"><div class="tDelete">Delete</div><div class="tClone">Clone</div></div>'); // Append the tool bar      
 	});
-	$('p,h1,h2,h3,h4').mouseenter(function(){
-		$(this).addClass('activeHover')
-		$(this).append('<div class="editIcon">Edit</div>')
+	
+	// Show the edit icon on editableElem mouse over
+	$(editableElem).mouseenter(function(){
+		$(this).addClass('activeHover');
+		$(this).append('<div class="editIcon">Edit</div>');
 	});
-	$('p,h1,h2,h3,h4').mouseleave(function(){
-		$(this).removeClass('activeHover')
+	
+	// Remove edit icon on editableElem mouse leave
+	$(editableElem).mouseleave(function(){
+		$(this).removeClass('activeHover');
 		$('body').find('.editIcon').remove();
 	});
+	
+	// Init JQTE plugin (WYSIWIG editor)
 	$('.jqte-test').jqte();
-	// Lets make the text editable
+	
+	// Bring up WYSIWIG editor and pass html into text area
 	$('body').on('click','.editIcon',function(){
-		var textTemp = $(this).parent().html();
-		$(this).parent().addClass('current');
-		$( "#dialog" ).dialog( "open" );
-		
-		$('.jqte_editor').html(textTemp);
-		
+		var textTemp = $(this).parent().html(); // Set variable to the HTML to be edited
+		$(this).parent().addClass('current'); // Add current class
+		$( "#dialog" ).dialog( "open" ); // Open jQuery UI modal window
+		$('.jqte_editor').html(textTemp); // Set the HTML of the modal text area to currently selected elem HTML
 	});
 	
-	$('body').on('click','.toolbox',function(){
-		alert('sdf');
-	});
-	
+	// Set some global settings for the jQuery UI modal window
 	$(function() {
-    $( "#dialog" ).dialog({
-      autoOpen: false,
-      show: {
-        effect: "fade",
-        duration: 200
-		
-      },
-      hide: {
-        effect: "fade",
-        duration: 200
-      }
-	  
+		$("#dialog").dialog({
+			autoOpen: false,
+			show: {
+				effect: "fade",
+				duration: modalDur
+			},
+			hide: {
+				effect: "fade",
+				duration: modalDur
+			}
+	    });
     });
  
- // Save edits button
- $('.saveE').click(function(){
-	var jqteIn = $('.jqte_editor').html();
-	$('.current').html(jqteIn);
-	$('.current').removeClass('current')
-	$( "#dialog" ).dialog( "close" );
+	// Modal save changes button
+	$('.saveE').click(function(){
+		var jqteIn = $('.jqte_editor').html(); // Set var to new HTML data
+		$('.current').html(jqteIn); // Inject this HTML into the selected current elem
+		$('.current').removeClass('current'); // Remove active class
+		$( "#dialog" ).dialog( "close" ); // Close jQuery UI dialog
 	});
-  
-  });
-  
-  $('.templateB').click(function(){
+	  
+	// Sidebar slide out
+	$('.templateB').click(function(){
 		$('#header').animate({'left':'200px'});
 		return false;
 	});
-	
 
-	});
+});
 	
